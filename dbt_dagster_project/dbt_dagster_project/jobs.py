97dd7cfdbd8272136756ce43ea38a36e60c0fe3a
dhaneesh88 <118976905+dhaneesh88@users.noreply.github.com>
2025-07-07 10:55:27 +0000

    Set default alert.

--- dbt_dagster_project/dbt_dagster_project/jobs.py
+++ dbt_dagster_project/dbt_dagster_project/jobs.py
@@ -1,11 +1,21 @@
 # jobs.py
-from dagster import job, op
+# from dagster import job, op
+# from .hooks import email_on_success, email_on_failure
+
+from dagster import define_asset_job
+from .assets import dbtlearn_dbt_assets
 from .hooks import email_on_success, email_on_failure
 
-@op
-def say_hello():
-    return "Hello every 5 minutes!"
+# @op
+# def say_hello():
+#     return "Hello every 5 minutes!"
 
-@job(hooks={email_on_success, email_on_failure})
-def every_five_minutes_job():
-    say_hello()
+# @job(hooks={email_on_success, email_on_failure})
+# def every_five_minutes_job():
+#     say_hello()
+
+every_five_minutes_job = define_asset_job(
+    name="every_five_minutes_job",
+    selection="*",  # materialize all assets
+    hooks={email_on_success, email_on_failure}
+)
